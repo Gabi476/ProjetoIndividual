caa5777e1c82fd75b58771a8c2f8d8e89974d78b
Gabi476 <gabriele.dutra@sptech.school>
2025-06-02 10:11:53 -0300

    Atualizações finais

--- api/web-data-viz/src/database/script-fairyinside.sql
+++ api/web-data-viz/src/database/script-fairyinside.sql
@@ -10,21 +10,36 @@ email varchar(200),
 senha char(10),
 genero varchar(45));
 
-create table forum(
-idfeed int not null auto_increment primary key,
-titulo varchar(100),
-data_feed date ,
-descricao varchar(255),
-fkusuario int,
-foreign key(fkusuario)references usuario (idusuario));
+create table filme(
+idfilme int not null auto_increment primary key,
+nomefilme varchar(100) not null);
+
+insert into filme(nomefilme)values
+('Tinker Bell'),
+('O tesouro perdido'),
+('O resgate da fada'),
+('O segredo das fadas'),
+('Fadas e piratas'),
+('O monstro da Terra do Nunca');
+
+create table avaliacao(
+    idavaliacao int not null auto_increment primary key,
+    fkusuario int not null,
+    fkfilme int not null,
+    nota decimal(2,1),
+    data_avaliacao datetime DEFAULT CURRENT_TIMESTAMP,
+    foreign key(fkusuario) references usuario(idusuario),
+    foreign key(fkfilme) references filme(idfilme)
+);
 
 create table quiz(
 idquiz int not null auto_increment,
 fkusuario int not null,
-resultado int not null,
-data_quiz date not null,
+resultado int,
+data_quiz  DATETIME DEFAULT CURRENT_TIMESTAMP,
+personalidade varchar(50),
 primary key(idquiz, fkusuario),
 foreign key(fkusuario) references usuario(idusuario)
 );
-
+show tables;
 select*from usuario;
