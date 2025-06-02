b673ceaf845c1eca38f3e36052268f920cb38c8a
Gabi476 <gabriele.dutra@sptech.school>
2025-06-02 11:02:03 -0300

    Final

--- api/web-data-viz/src/routes/quiz1.js
+++ api/web-data-viz/src/routes/quiz1.js
@@ -1,7 +1,7 @@
 var express= require("express");    
 var router=express.Router();
 
-var quiz1Controller=require("../controllers/quiz1Controller");
+var quiz1Controller=require("../controllers/quiz1Controller"); 
 
 router.post("/inserirPontuacao", function(req, res){
     quiz1Controller.inserir(req, res);
